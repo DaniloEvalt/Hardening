17b0d2f265a0b41e5646b86a2ee7c2b3c23c2555
Danilo <daniloguizardi@gmail.com>
2020-07-08 11:20:14 -0300

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -3,11 +3,25 @@
 echo "Atualizando o sistema!"
 yum update -y && yum upgrde -y
 
+echo "Instalando dependências"
+yum install policycoreutils-python epel-release fail2ban fail2ban-systemd -y
+yum update -y selinux-policy*
+
+systemctl enable firewalld
+systemctl start firewalld
+
+systemctl enable fail2ban
+systemctl start fail2ban 
+
+
+cp -pf /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+wget -c https://raw.githubusercontent.com/DaniloEvalt/ShellScript/blob/master/jail.local >> /etc/fail2ban/jail.local
+
+wget -c https://raw.githubusercontent.com/DaniloEvalt/ShellScript/blob/master/sshd.local >> /etc/fail2ban/jail.d/sshd.local
+
 echo "Alterando a porta do SSH"
 sed -i 's,# Port 22,Port 6022,g' /etc/ssh/sshd_config
-
-echo "Instalando dependências"
-yum install policycoreutils-python
 
 echo "Adicionando liberação"
 semanage port -a -t ssh_port_t -p tcp 6022
@@ -18,25 +32,6 @@ firewall-cmd --reload
 
 echo "Reiniciando o SSH"
 systemctl restart sshd
-
-echo "Instalando e configurando o FAIL2BAN"
-yum install epel-release -y
-yum install fail2ban fail2ban-systemd -y
-
-yum update -y selinux-policy*
-
-cp -pf /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-
-wget -c https://raw.githubusercontent.com/DaniloEvalt/ShellScript/blob/master/jail.local >> /etc/fail2ban/jail.local
-
-wget -c https://raw.githubusercontent.com/DaniloEvalt/ShellScript/blob/master/sshd.local >> /etc/fail2ban/jail.d/sshd.local
-
-systemctl enable firewalld
-systemctl start firewalld
-
-systemctl enable fail2ban
-systemctl start fail2ban 
-
 
 echo "Desabilitando o login ssh do usuário ROOT"
 sed -i 's,# PermitRootLogin yes,PermitRootLogin no,g' /etc/ssh/sshd_config
